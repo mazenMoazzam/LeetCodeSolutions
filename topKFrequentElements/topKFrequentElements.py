a6ac155d35e21eccd21cc39bbb4bb756d866979a
mazenMoazzam <137253449+mazenMoazzam@users.noreply.github.com>
2024-08-27 12:07:13 -0400

    Update topKFrequentElements.py

--- topKFrequentElements/topKFrequentElements.py
+++ topKFrequentElements/topKFrequentElements.py
@@ -16,14 +16,17 @@ class Solution(object):
                 else:
                     freqCount[x] = 1
             return freqCount
+        #decided to intialize a hashmap to count the frequencies for each element that will be inserted into the heap. O(n) time operation.
 
         def topK(array, k):
             counter = countElements(array)
-            heap = [(-freq, num) for num, freq in counter.items()]
+            heap = [(-freq, num) for num, freq in counter.items()] #as python does not provide maxHeaps, I decided to use a min heap and negate the frequencies,
+            #as the minimum frequency will be still be the same value when it is a positive number. 
             heapq.heapify(heap)
             topElements = [heapq.heappop(heap)[1] for x in range(k)]
 
             return topElements
         return topK(nums, k) 
+        
 #runtime of code: O(N + m + k log m)
         
