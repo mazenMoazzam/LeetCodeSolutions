b8cc3dd71568cb12a605c07ad9efece6c0f9345c
Mazen Moazzam <137253449+mazenMoazzam@users.noreply.github.com>
2024-09-22 01:20:22 -0400

    Update palindromeLinked.py

--- palindromeLinkedList/palindromeLinked.py
+++ palindromeLinkedList/palindromeLinked.py
@@ -19,3 +19,6 @@ class Solution(object):
         
         return string == string[::-1]
 
+#Time Complexity: O(n)
+#Space complexity: O(n) due to string definition on line 12, can make constant space by modifying linkedlist by finding the middle element and compare the first half before middle element and second half after middle element
+#and compare to see if they are the same, if they are, the linked list is a palindrome, otherwise, return false. 
