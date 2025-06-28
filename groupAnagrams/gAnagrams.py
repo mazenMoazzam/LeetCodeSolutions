e85d95d3a915813533b7502e36c0d9f6d7d76ef4
Mazen Moazzam <137253449+mazenMoazzam@users.noreply.github.com>
2025-06-27 21:02:10 -0400

    Update gAnagrams.py

--- groupAnagrams/gAnagrams.py
+++ groupAnagrams/gAnagrams.py
@@ -19,3 +19,6 @@ class Solution(object):
 
 
 #O(n) time, due to traversing through n elements in the list which represent the amount of strings.
+#General approach is to create a dictionary which the key will represent the string sorted in order, which will help distinguish what string is an anagram or not
+#with this, we travrse through the list to sort the current string, we check if this string is in the anagram dictionary, if it is, we simply add it to the list value
+#of the dictionary, if not er vreate a list with the first element being the current iteration of the string.
