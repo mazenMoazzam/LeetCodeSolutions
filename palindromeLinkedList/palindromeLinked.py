6e15f8c6c3b8a936a25e9013ad4b1ad5950833a7
Mazen Moazzam <137253449+mazenMoazzam@users.noreply.github.com>
2025-06-27 21:15:46 -0400

    Update palindromeLinked.py

--- palindromeLinkedList/palindromeLinked.py
+++ palindromeLinkedList/palindromeLinked.py
@@ -14,7 +14,7 @@ class Solution(object):
         current = head
 
         while current != None:
-            string += str(current.val)
+            string += str(current.val)l
             current = current.next
         
         return string == string[::-1] 
