6a52fc46e6bac97eb55843e0ad3a540fae1952be
Mazen Moazzam <137253449+mazenMoazzam@users.noreply.github.com>
2025-07-20 23:15:17 -0400

    Update houseRobber.py

--- HouseRobber/houseRobber.py
+++ HouseRobber/houseRobber.py
@@ -17,3 +17,8 @@ class Solution(object):
             dp0 = dp1
             dp1 = current
         return dp1
+#Basic DP problem where we want to find the maximum amount of money we can steal from each house which is represent in nums.
+#Defined dp0 to represent i - 2 and dp1 which equals i - 1. (houses before previous and previous house). These represent starting points, and will iterate
+#to the third house till the end and see if we can skip the current house and take the previous house value, or take the current house and add that to the i - 2
+# house, we take the max of both. To iterate to next houses, we reassign dp0 and dp1, dp0 = dp1 (which is now 2 houses down) and dp1 = current which is i - 1.
+#We return dp1, which contains the max value of earnings.
